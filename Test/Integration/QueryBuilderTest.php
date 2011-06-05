d2717fa490febdcea77e33492dbc9029cd0c858d
Alessandro Nadalin <alessandro.nadalin@gmail.com>
2011-06-05 17:58:41 +0200

    Fully tested execution of SELECTs with the QB.

--- Test/Integration/QueryBuilderTest.php
+++ Test/Integration/QueryBuilderTest.php
@@ -38,27 +38,92 @@ class QueryBuilderTest extends TestCase
   {
       $this->query->from(array('address'));
       
-      $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw())->getStatusCode());
+      $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
       
       $this->query->select(array('@version','street'));
       
-      $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw())->getStatusCode());
+      $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
   }
   
-  public function testAComplexSelect()
+  public function testASelectThoughAnIndex()
   {
     $query = new Query();
     $query->index('index_name','unique');
     
-    $this->assertStatusCode(self::_204, $this->orient->command($query->getRaw())->getStatusCode());
+    $this->assertStatusCode(self::_204, $this->orient->command($query->getRaw()));
     
     $this->query->from(array('index:index_name'))->between('key','10.0','10.1');
-    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw())->getStatusCode());
+    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
     
     $query = new Query();
     $query->unindex('index_name');
     
-    $this->assertStatusCode(self::_204, $this->orient->command($query->getRaw())->getStatusCode());    
-    
+    $this->assertStatusCode(self::_204, $this->orient->command($query->getRaw()));    
+  }
+
+  public function testTheRangeOfASelect()
+  {
+    $this->query->from(array('Address'))->range('12:1');
+
+    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
+
+    $this->query->range(null, '12');
+
+    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
+
+    $this->query->range('10.0');
+
+    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
+
+    $this->query->range('10.1', false);
+
+    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
+
+    $this->query->range('10.1', '10.2');
+
+    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
+  }
+
+  public function testLimitingASelect()
+  {
+    $this->query->from(array('Address'))->limit(20);
+
+    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
+
+    $this->query->from(array('Address'))->limit('20');
+
+    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
+
+    $this->query->from(array('Address'))->limit('a');
+
+    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
+  }
+
+  public function testSelectingByRIDs()
+  {
+    $this->query->from(array('12:0', '12:1'));
+
+    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
+  }
+
+  public function testOrderingTheQuery()
+  {
+    $this->query->from(array('12:0', '12:1'))->orderBy('rid ASC')->orderBy('street DESC');
+
+    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
+  }
+
+  public function testDoingAComplexSelect()
+  {
+    $this->query->limit(10);
+    $this->query->limit(20);
+    $this->query->from(array('12:2', '12:4'), false);
+    $this->query->select(array('rid', 'street'));
+    $this->query->select(array('type'));
+    $this->query->range('12:2');
+    $this->query->range(null, '12:4');
+    $this->query->orderBy('street ASC');
+
+    $this->assertStatusCode(self::_200, $this->orient->command($this->query->getRaw()));
   }
 }
