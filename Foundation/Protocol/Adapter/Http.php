21121826110f3305f6ee1b03b0a6b0fb854a91b0
Alessandro Nadalin <alessandro.nadalin@gmail.com>
2011-08-03 00:40:11 +0200

    Added a first implementation of the HTTP protocol adapter which will be used by the data mapper to retrieve data.

---
+++ Foundation/Protocol/Adapter/Http.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Orient package.
+ *
+ * (c) Alessandro Nadalin <alessandro.nadalin@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Class Http
+ *
+ * @package     
+ * @subpackage  
+ * @author      Alessandro Nadalin <alessandro.nadalin@gmail.com>
+ */
+
+namespace Orient\Foundation\Protocol\Adapter;
+
+use Orient\Contract\Protocol\Adapter as ProtocolAdapter;
+use Orient\Contract\Http\Client;
+use Orient\Foundation\Binding;
+
+class Http implements protocolAdapter
+{
+    /**
+     * Instantiates a new adapter.
+     *
+     * @api
+     * @param Http\Client $client
+     * @param String $host
+     * @param String $port
+     * @param String $username
+     * @param String $password
+     * @todo better to inject the binding
+     */
+    public function __construct(Client $client, $host = '127.0.0.1', $port = 2480, $username = null, $password = null, $database = null)
+    {
+        $this->client = new Binding($client, $host, $port, $username, $password, $database);
+    }
+    
+    /**
+     * Executes a command against OrientDB via the HTTP binding.
+     *
+     * @param   type $command
+     * @return  mixed
+     * @throws  \Exception
+     * @todo should return StdObject for SELECT and stuff that retrieves data, true otherwise
+     * @todo throw a specific exception
+     * @exploding the status code should not be done hete, need to add an option to
+     * ->getStatusCode($numeric) to return only 200 instead of HTTP/1.1 200 OK
+     */
+    public function execute($command)
+    {
+        $response = $this->client->command($command);
+        
+        if (!$response) {
+            throw new \Exception('Unable to retrieve a response');
+        }
+        
+        $statusCode = explode(' ', $response->getStatusCode());
+        
+        /**
+         * @todo ugly
+         */
+        if ($statusCode[1][0] == 2) {
+            return true;
+        }
+        
+        throw new \Exception($response->getBody());
+    }
+}
+
