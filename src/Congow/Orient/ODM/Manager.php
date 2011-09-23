2f77966896f87ea89182eca960a302b64acc3333
Alessandro Nadalin <alessandro.nadalin@gmail.com>
2011-09-23 21:48:13 +0200

    implemented Manager as a Proxy for Mapper's public methods

--- src/Congow/Orient/ODM/Manager.php
+++ src/Congow/Orient/ODM/Manager.php
@@ -22,8 +22,9 @@
 namespace Congow\Orient\ODM;
 
 use Congow\Orient\ODM\Mapper;
+use Doctrine\Common\Persistence\ObjectManager;
 
-class Manager
+class Manager implements ObjectManager
 {
     protected $mapper;
     
@@ -63,4 +64,74 @@ class Manager
     {
         $this->mapper->setDocumentDirectories($directories);
     }
+    
+    /**
+     * Enable or disable overflows' tolerance.
+     *
+     * @see   toleratesOverflow()
+     * @param boolean $value 
+     */
+    public function enableOverflows($value = true)
+    {
+        $this->mapper->enableOverflows($value);
+    }
+
+    /**
+     * Via a protocol adapter, it queries for an object with the given $rid.
+     * If $lazy loading is used, all of this won't be executed unless the
+     * returned Proxy object is called via __invoke, e.g.:
+     * 
+     * <code>
+     *   $lazyLoadedRecord = $mapper->find('1:1', true);
+     * 
+     *   $record = $lazyLoadedRecord();
+     * </code>
+     *
+     * @param string    $rid
+     * @param boolean   $lazy
+     * @return Proxy|object
+     */
+    public function find($rid, $lazy = false){
+        return $this->mapper->find($rid, $lazy);
+    }
+    
+    /**
+     * Via a protocol adapter, it queries for an array of objects with the given
+     * $rids.
+     * If $lazy loading is used, all of this won't be executed unless the
+     * returned Proxy object is called via __invoke, e.g.:
+     * 
+     * <code>
+     *   $lazyLoadedRecords = $mapper->find('1:1', true);
+     * 
+     *   $records = $lazyLoadedRecord();
+     * </code>
+     *
+     * @param string    $rid
+     * @param boolean   $lazy
+     * @return Proxy\Collection|array
+     */
+    public function findRecords(Array $rids, $lazy = false){
+        return $this->mapper->findRecords($rids, $lazy);
+    }
+    
+    /**
+     * Returns the internal object used to parse annotations.
+     *
+     * @return AnnotationReader
+     */
+    public function getAnnotationReader()
+    {
+        return $this->mapper->getAnnotationReader();
+    }
+    
+    /**
+     * @param   array $json
+     * @return  array of Documents
+     */
+    public function hydrateCollection(array $collection)
+    {
+        return $this->mapper->hydrateCollection($collection);
+    }
+    
 }
