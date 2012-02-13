c3b0204dbc6095cd2fc0b5751384949a64bdf058
Alessandro Nadalin <alessandro.nadalin@gmail.com>
2012-02-13 15:13:55 +0100

    started the setup for data types integration tests

---
+++ test/Integration/ODM/Mapper/DataType/StringTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * QueryTest
+ *
+ * @package    Congow\Orient
+ * @subpackage Test
+ * @author     Alessandro Nadalin <alessandro.nadalin@gmail.com>
+ * @author     David Funaro <ing.davidino@gmail.com>
+ * @version
+ */
+
+namespace test\Integration\ODM\Mapper\DataType;
+
+use test\PHPUnit\TestCase;
+
+class StringTest extends TestCase
+{
+  public function testHydratingAStringProperty()
+  {
+    $this->markTestIncomplete();
+  }
+}
