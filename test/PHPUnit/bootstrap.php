db3e681d530184efc8e86adcccf495282b4826b5
Alessandro Nadalin <alessandro.nadalin@gmail.com>
2012-08-17 12:41:17 +0400

    Moved proxy classes used for the tests in the test folder.

--- test/PHPUnit/bootstrap.php
+++ test/PHPUnit/bootstrap.php
@@ -7,7 +7,7 @@ require_once(__DIR__.'/../../autoload.php');
 $loader = new UniversalClassLoader();
 
 $loader->registerNamespaces(array(
-    'Congow\Orient\Proxy' => __DIR__.'/../../proxies/',
+    'Congow\Orient\Proxy' => __DIR__.'/../../test/proxies/',
     'test' => __DIR__.'/../../',
 ));
 
