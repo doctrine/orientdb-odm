133fede9f5b19dcaf6409c11995f67fbd2e1840c
Daniele Alessandri <suppakilla@gmail.com>
2012-12-23 14:39:56 +0100

    Adapt CI scripts to OrientDB distribution packages changes.
    Starting with OrientDB v1.3.0 they changed URL for downloadable packages
    moving from Google Code to Amazon S3, but leaving old versions available
    only from the former location. They also switched to tar.gz from zip.
    
    This also fixes a bug due to non-existing output directory when using
    cURL to download the compressed archive.

--- bin/initialize-ci.sh
+++ bin/initialize-ci.sh
@@ -4,8 +4,7 @@ PARENT_DIR=$(dirname $(cd "$(dirname "$0")"; pwd))
 CI_DIR="$PARENT_DIR/ci-stuff/environment"
 
 ODB_VERSION=${1:-"1.2.0"}
-ODB_PACKAGE="orientdb-${ODB_VERSION}"
-ODB_DIR="${CI_DIR}/${ODB_PACKAGE}"
+ODB_DIR="${CI_DIR}/orientdb-${ODB_VERSION}"
 ODB_LAUNCHER="${ODB_DIR}/bin/server.sh"
 
 echo "=== Initializing CI environment ==="
@@ -14,18 +13,18 @@ cd "$PARENT_DIR"
 
 . "$PARENT_DIR/bin/odb-shared.sh"
 
-if [ ! -d "$CI_DIR" ]; then
-  # Download and extract OrientDB
+if [ ! -d "$ODB_DIR" ]; then
+  # Download and extract OrientDB server
   echo "--- Downloading OrientDB v${ODB_VERSION} ---"
-  odb_download "https://orient.googlecode.com/files/${ODB_PACKAGE}.zip" $CI_DIR
-  unzip -q "${CI_DIR}/${ODB_PACKAGE}.zip" -d $CI_DIR && chmod +x $ODB_LAUNCHER
+  odb_download_server $ODB_VERSION $CI_DIR
 
-  # Copy the configuration file and the demo database
+  # Ensure that launcher script is executable and copy configurations file
   echo "--- Setting up OrientDB ---"
+  chmod +x $ODB_LAUNCHER
   cp $PARENT_DIR/ci-stuff/orientdb-server-config.xml "${ODB_DIR}/config/"
   cp $PARENT_DIR/ci-stuff/orientdb-server-log.properties "${ODB_DIR}/config/"
 else
-  echo "!!! Directory $CI_DIR exists, skipping downloads !!!"
+  echo "!!! Found OrientDB v${ODB_VERSION} in ${ODB_DIR} !!!"
 fi
 
 # Start OrientDB in background.
--- bin/odb-shared.sh
+++ bin/odb-shared.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
+odb_compare_version () {
+  # TODO: this function does not handle well versions with additional rank
+  # indicators such as "-rc1" but I guess it suffices for now.
+  GTVER=$(echo "$1\n$2" | sort -n --reverse | head -1)
+
+  if [ $1 = $2 ]; then
+    echo 0
+  elif [ $1 = $GTVER ]; then
+    echo 1
+  else
+    echo -1
+  fi
+}
+
 odb_command_exists () {
   type "$1" >/dev/null 2>&1 ;
 }
 
 odb_download () {
   OUTPUT_DIR="${2:-$(pwd)}"
+
+  if [ ! -d "$OUTPUT_DIR" ]; then
+    mkdir "$OUTPUT_DIR"
+  fi
 
   if odb_command_exists "wget" ; then
     wget -q -P "$OUTPUT_DIR" "$1"
@@ -14,5 +32,33 @@ odb_download () {
   else
     echo "Cannot download $1 [missing wget or curl]"
     exit 1
+  fi
+}
+
+odb_download_server () {
+  ODB_VERSION=$1
+  CI_DIR=$2
+
+  ODB_PACKAGE="orientdb-${ODB_VERSION}"
+
+  # We need to resort to tricks to automate our CI environment as much as
+  # possible since the OrientDB guys keep changing the compressed archive
+  # format and moving the downloadable packages URLs. Luckily for us, we
+  # are smart enough to cope with that... at least until the next change.
+  if [ $(odb_compare_version $ODB_VERSION 1.3.0) -ge 0 ]; then
+    ODB_PACKAGE_EXT="tar.gz"
+    ODB_PACKAGE_URL="https://s3.amazonaws.com/orientdb/releases/${ODB_PACKAGE}.${ODB_PACKAGE_EXT}"
+  else
+    ODB_PACKAGE_EXT="zip"
+    ODB_PACKAGE_URL="https://orient.googlecode.com/files/${ODB_PACKAGE}.${ODB_PACKAGE_EXT}"
+  fi
+
+  odb_download $ODB_PACKAGE_URL $CI_DIR
+  ODB_PACKAGE_PATH="${CI_DIR}/${ODB_PACKAGE}.${ODB_PACKAGE_EXT}"
+
+  if [ $ODB_PACKAGE_EXT = "zip" ]; then
+    unzip -q $ODB_PACKAGE_PATH -d ${CI_DIR}
+  elif [ $ODB_PACKAGE_EXT = "tar.gz" ]; then
+    tar xf $ODB_PACKAGE_PATH -C $CI_DIR
   fi
 }
