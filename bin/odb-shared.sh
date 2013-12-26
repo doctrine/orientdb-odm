6f103a235711b7241b825535e961614f928c9b08
David Funaro <ing.davidino@gmail.com>
2013-12-26 01:47:44 +0100

    fix init scripts

--- bin/odb-shared.sh
+++ bin/odb-shared.sh
@@ -35,6 +35,23 @@ odb_download () {
   fi
 }
 
+odb_download_composer () {
+  OUTPUT_DIR="${2:-$(pwd)}"
+
+  if [ ! -d "$OUTPUT_DIR" ]; then
+    mkdir "$OUTPUT_DIR"
+  fi
+
+  if odb_command_exists "wget" ; then
+    wget -q -P "$OUTPUT_DIR" "$1"
+  elif odb_command_exists "curl" ; then
+    ( cd "$OUTPUT_DIR" > /dev/null ; curl --silent -O "$1" ; )
+  else
+    echo "Cannot download $1 [missing wget or curl]"
+    exit 1
+  fi
+}
+
 odb_download_server () {
   #http://www.orientdb.org/portal/function/portal/download/phpuser@unknown.com/%20/%20/%20/%20/unknown/orientdb-community-1.6.2.tar.gz/false/false
 
