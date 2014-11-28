e029f23e7c57ece28fc399ce1c7382f54a852e9d
mitom <tamas.millian@gmail.com>
2014-11-28 14:58:04 +0100

    fixed invalid annotation

--- src/Doctrine/ODM/OrientDB/Proxy/Proxy.php
+++ src/Doctrine/ODM/OrientDB/Proxy/Proxy.php
@@ -3,12 +3,10 @@
 namespace Doctrine\ODM\OrientDB\Proxy;
 
 use Doctrine\Common\Proxy\Proxy as BaseProxy;
+
 /**
- * Document Proxy interface.
- *
- * @since       1.0
- * @author      Jonathan H. Wage <jonwage@gmail.com>
- * @author      Roman Borschel <roman@code-factory.org>
- * @author      Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
+ * Interface Proxy
+ * Placeholder so if the interface is extended it won't
+ * break the current implementations
  */
 interface Proxy extends BaseProxy {}
